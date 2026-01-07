fb95d4e6f073cecffa0c63de762aace1993e58dd
maxiscritikal <101420657+maxiscritikal@users.noreply.github.com>
2026-01-07 16:09:40 +0000

    commit 1

--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -2,5 +2,14 @@ public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
 
+    public car(String prod, String org, String des, int w, int m)
+    {
+        product = prod;
+        origin = org;
+        destination = des;
+        weight = w;
+        miles = m;
+    }
+
 
 }
